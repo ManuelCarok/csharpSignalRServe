c64535a9ee1992586e389965529e9c650e6af7cd
Manuel Caroca <manuelcarok@gmail.com>
2019-06-30 23:11:06 -0400

    Proyecto con las funciones basicas de un chat con signalr

---
+++ SignalRTest/Hubs/ChatHub.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace SignalRTest.Hubs
+{
+    [HubName("chat")]
+    public class ChatHub : Hub
+    {
+        public static List<Client> ConnectedUsers { get; set; } = new List<Client>();
+
+        public void Connect(string username) {
+            Client client = new Client() {
+                UserName = username,
+                IdUser = Context.ConnectionId
+            };
+            ConnectedUsers.Add(client);
+            Clients.All.updateUsers(ConnectedUsers.Count(), ConnectedUsers.Select(u => u.UserName));
+        }
+
+        public void Send(string message)
+        {
+            var sender = ConnectedUsers.First(u => u.IdUser.Equals(Context.ConnectionId));
+            Clients.All.broadcastMessage(sender.UserName, message);
+        }
+
+        public override Task OnDisconnected(bool stopCalled)
+        {
+            var disconnectedUser = ConnectedUsers.FirstOrDefault(c => c.IdUser.Equals(Context.ConnectionId));
+            ConnectedUsers.Remove(disconnectedUser);
+            Clients.All.updateUsers(ConnectedUsers.Count(), ConnectedUsers.Select(u => u.UserName));
+            return base.OnDisconnected(stopCalled);
+        }
+    }
+
+    public class Client
+    {
+        public string UserName { get; set; }
+        public string IdUser { get; set; }
+    }
+}
---
+++ SignalRTest/Startup.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.Owin.Cors;
+using Owin;
+
+namespace SignalRTest
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app) {
+            app.Map("/signalr", map =>
+            {
+                map.UseCors(CorsOptions.AllowAll);
+                var hubConfiguration = new HubConfiguration
+                {
+                    // EnableJSONP = true
+                };
+                map.RunSignalR(hubConfiguration);
+            });
+        }
+    }
+}
